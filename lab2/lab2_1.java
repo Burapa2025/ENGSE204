bf21f4f814da31b5501eb560fbc012ab9ce1eb6f
Burapa <guy6176@gmail.com>
2025-12-03 19:56:28 +0700

    Update lab2_1.java

--- lab2/lab2_1.java
+++ lab2/lab2_1.java
@@ -1,29 +1,30 @@
-package lab2;
+package lab2 ;
 
-import java.util.Scanner;
+import java.util.Scanner ;
 
 class Student {
-    String studentId;
-    String name;
+    String studentId ;
+    String name ;
 }
 
 public class lab2_1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in) ;
 
-        System.out.print("Enter student ID: ");
-        String studentId = scanner.nextLine();
+        System.out.print("Enter student ID: ") ;
+        String studentId = scanner.nextLine() ;
 
-        System.out.print("Enter student name: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter student name: ") ;
+        String name = scanner.nextLine() ;
 
-        Student student = new Student();
-        student.studentId = studentId;
-        student.name = name;
+        Student student = new Student() ;
+        student.studentId = studentId ;
+        student.name = name ;
 
-        System.out.println("Student ID: " + student.studentId);
-        System.out.println("Student Name: " + student.name);
+        System.out.println("Student ID: " + student.studentId) ;
+        System.out.println("Student Name: " + student.name) ;
 
-        scanner.close();
+        scanner.close() ;
     }
 }
+
